a457980f0b9c685814b0b8d4c460d2b5d0961b71
Amira Azeer <103902057+amira-azeer@users.noreply.github.com>
2026-02-10 15:03:50 +0530

    Create Stacks.js

---
+++ Stacks.js
@@ -0,0 +1,67 @@
+class Node {
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor(){
+        this.top = null;
+        this.bottom = null;
+        this.length = 0
+    }
+    
+    peek(){ // view the top item on the stack
+        return this.top
+    }
+    
+    push(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++
+        return this
+    }
+    
+    pop(){
+        if(!this.top){
+            return null;
+        }
+        if(this.top === this.bottom){
+            this.bottom = null
+        }
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+    
+}
+const myStack = new Stack()
+
+class ArrayStack {
+    constructor(){
+       this.array = [];
+    }
+    
+    peek(){ // view the top item on the stack
+        return this.array[this.array.length-1]
+    }
+    
+    push(value){
+        this.array.push(value);
+        return this;
+    }
+    
+    pop(){
+        this.array.pop()
+    }
+    
+}
+const myArrStack = new ArrayStack()
